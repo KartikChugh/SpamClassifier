ad41f8e6807e16b965edb4a1b1b4e954c4cbba07
KartikChugh <kartikchugh.kc@gmail.com>
2020-04-17 15:15:54 -0400

    Fix decoding error when reading data

--- models/model.py
+++ models/model.py
@@ -8,7 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 
-df = pd.read_csv('models/saved/dataframe.csv')
+df = pd.read_csv('models/saved/dataframe.csv', encoding = "ISO-8859-1")
 
 # Split training/test sets
 
@@ -17,7 +17,7 @@ y = df['label']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Build classifier model
+# Build classifier
 
 clf = Pipeline(steps=[
     ('tfidf', TfidfVectorizer()),
