76cf2ca81352bc4577d9ff96171ea7b560b137f5
KartikChugh <kartikchugh.kc@gmail.com>
2020-04-17 14:36:04 -0400

    Split training and test sets

--- models/model.py
+++ models/model.py
@@ -7,3 +7,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv('models/saved/dataframe.csv')
+
+X = df['message']
+y = df['label']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
