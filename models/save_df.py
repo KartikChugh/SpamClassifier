c2a2e650bfee4d02f499d1134102b5b0e9e53c21
KartikChugh <kartikchugh.kc@gmail.com>
2020-04-17 13:14:11 -0400

    Clean SMS dataset

--- models/save_df.py
+++ models/save_df.py
@@ -4,4 +4,12 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
 df = pd.read_csv('https://raw.githubusercontent.com/mohitgupta-omg/Kaggle-SMS-Spam-Collection-Dataset-/master/spam.csv', encoding = "ISO-8859-1")
+
+df.rename(columns={
+    'v1': 'label',
+    'v2': 'message',
+}, inplace=True)
+
+df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
+
 print(df.head())
